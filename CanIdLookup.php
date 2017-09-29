89f52580bac6c39c691a55bf8d384d9641f31077
Michael Chora <michael.chora@gmail.com>
2017-09-29 21:33:39 +1000

    no message

--- CanIdLookup.php
+++ CanIdLookup.php
@@ -70,77 +70,81 @@ $CanIdArray = array(
                             "3" => array("Position" => "6,7", "Name"=> "IntakeCamAngle2", "Units" => "Degrees", "Conversion" => "x/10")
                         ), 
                 "371" => array (
-                            "0,1" => array("Name"=> "FuelFlow", "Units" => "cc/min", "Conversion" => "x"),
-                            "2,3" => array("Name"=> "FuelFlowReturn", "Units" => "cc/min", "Conversion" => "x")	
+                            "0" => array("Position" => "0,1", "Name"=> "FuelFlow", "Units" => "cc/min", "Conversion" => "x"),
+                            "1" => array("Position" => "2,3", "Name"=> "FuelFlowReturn", "Units" => "cc/min", "Conversion" => "x")	
                         ),    
                 "372" => array (
-                            "0,1" => array("Name"=> "BatteryVoltage", "Units" => "Volts", "Conversion" => "x/10"),
-                            "4,5" => array("Name"=> "TargetBoostLevel", "Units" => "kPa", "Conversion" => "x/10"),
-                            "6,7" => array("Name"=> "BarometricPressure", "Units" => "kPa", "Conversion" => "x/10")
+                            "0" => array("Position" => "0,1", "Name"=> "BatteryVoltage", "Units" => "Volts", "Conversion" => "x/10"),
+                            "1" => array("Position" => "4,5", "Name"=> "TargetBoostLevel", "Units" => "kPa", "Conversion" => "x/10"),
+                            "2" => array("Position" => "6,7", "Name"=> "BarometricPressure", "Units" => "kPa", "Conversion" => "x/10")
                         ), 
                 "373" => array (
-                            "0,1" => array("Name"=> "EGTSensor1", "Units" => "DegreesK", "Conversion" => "x/10"),
-                            "2,3" => array("Name"=> "EGTSensor2", "Units" => "DegreesK", "Conversion" => "x/10"),	
-                            "4,5" => array("Name"=> "EGTSensor3", "Units" => "DegreesK", "Conversion" => "x/10"),
-                            "6,7" => array("Name"=> "EGTSensor4", "Units" => "DegreesK", "Conversion" => "x/10")
+                            "0" => array("Position" => "0,1", "Name"=> "EGTSensor1", "Units" => "DegreesK", "Conversion" => "x/10"),
+                            "1" => array("Position" => "2,3", "Name"=> "EGTSensor2", "Units" => "DegreesK", "Conversion" => "x/10"),	
+                            "2" => array("Position" => "4,5", "Name"=> "EGTSensor3", "Units" => "DegreesK", "Conversion" => "x/10"),
+                            "3" => array("Position" => "6,7", "Name"=> "EGTSensor4", "Units" => "DegreesK", "Conversion" => "x/10")
                         ),   
                 "374" => array (
-                            "0,1" => array("Name"=> "EGTSensor5", "Units" => "DegreesK", "Conversion" => "x/10"),
-                            "2,3" => array("Name"=> "EGTSensor6", "Units" => "DegreesK", "Conversion" => "x/10"),	
-                            "4,5" => array("Name"=> "EGTSensor7", "Units" => "DegreesK", "Conversion" => "x/10"),
-                            "6,7" => array("Name"=> "EGTSensor8", "Units" => "DegreesK", "Conversion" => "x/10")
+                            "0" => array("Position" => "0,1", "Name"=> "EGTSensor5", "Units" => "DegreesK", "Conversion" => "x/10"),
+                            "1" => array("Position" => "2,3", "Name"=> "EGTSensor6", "Units" => "DegreesK", "Conversion" => "x/10"),	
+                            "2" => array("Position" => "4,5", "Name"=> "EGTSensor7", "Units" => "DegreesK", "Conversion" => "x/10"),
+                            "3" => array("Position" => "6,7", "Name"=> "EGTSensor8", "Units" => "DegreesK", "Conversion" => "x/10")
                         ),   
                 "375" => array (
-                            "0,1" => array("Name"=> "EGTSensor9", "Units" => "DegreesK", "Conversion" => "x/10"),
-                            "2,3" => array("Name"=> "EGTSensor10", "Units" => "DegreesK", "Conversion" => "x/10"),	
-                            "4,5" => array("Name"=> "EGTSensor11", "Units" => "DegreesK", "Conversion" => "x/10"),
-                            "6,7" => array("Name"=> "EGTSensor12", "Units" => "DegreesK", "Conversion" => "x/10")
+                            "0" => array("Position" => "0,1", "Name"=> "EGTSensor9", "Units" => "DegreesK", "Conversion" => "x/10"),
+                            "1" => array("Position" => "2,3", "Name"=> "EGTSensor10", "Units" => "DegreesK", "Conversion" => "x/10"),	
+                            "2" => array("Position" => "4,5", "Name"=> "EGTSensor11", "Units" => "DegreesK", "Conversion" => "x/10"),
+                            "3" => array("Position" => "6,7", "Name"=> "EGTSensor12", "Units" => "DegreesK", "Conversion" => "x/10")
                         ),          
                 "3E0" => array (
-                            "0,1" => array("Name"=> "CoolantTemperature", "Units" => "DegreesK", "Conversion" => "x/10"),
-                            "2,3" => array("Name"=> "AirTemperature", "Units" => "DegreesK", "Conversion" => "x/10"),	
-                            "4,5" => array("Name"=> "FuelTemperature", "Units" => "DegreesK", "Conversion" => "x/10"),
-                            "6,7" => array("Name"=> "OilTemperature", "Units" => "DegreesK", "Conversion" => "x/10")
+                            "0" => array("Position" => "0,1", "Name"=> "CoolantTemperature", "Units" => "DegreesK", "Conversion" => "x/10"),
+                            "1" => array("Position" => "2,3", "Name"=> "AirTemperature", "Units" => "DegreesK", "Conversion" => "x/10"),	
+                            "2" => array("Position" => "4,5", "Name"=> "FuelTemperature", "Units" => "DegreesK", "Conversion" => "x/10"),
+                            "3" => array("Position" => "6,7", "Name"=> "OilTemperature", "Units" => "DegreesK", "Conversion" => "x/10")
                         ),      
                  "3E1" => array (
-                            "0,1" => array("Name"=> "GearboxOilTemperature", "Units" => "DegreesK", "Conversion" => "x/10"),
-                            "2,3" => array("Name"=> "DiffTemperature", "Units" => "DegreesK", "Conversion" => "x/10"),	
-                            "4,5" => array("Name"=> "FuelConsumption", "Units" => "%", "Conversion" => "x/10")
+                            "0" => array("Position" => "0,1", "Name"=> "GearboxOilTemperature", "Units" => "DegreesK", "Conversion" => "x/10"),
+                            "1" => array("Position" => "2,3", "Name"=> "DiffTemperature", "Units" => "DegreesK", "Conversion" => "x/10"),	
+                            "2" => array("Position" => "4,5", "Name"=> "FuelConsumption", "Units" => "%", "Conversion" => "x/10")
                         ),  
                  "3E2" => array (
-                            "0,1" => array("Name"=> "FuelLevel", "Units" => "L", "Conversion" => "x/10")
+                            "0" => array("Position" => "0,1", "Name"=> "FuelLevel", "Units" => "L", "Conversion" => "x/10")
                         ),     
                  "3E3" => array (
-                            "0,1" => array("Name"=> "FuelTrimShortTermBank1", "Units" => "%", "Conversion" => "x/10"),
-                            "2,3" => array("Name"=> "FuelTrimShortTermBank1", "Units" => "%", "Conversion" => "x/10"),	
-                            "4,5" => array("Name"=> "FuelTrimLongTermBank1", "Units" => "%", "Conversion" => "x/10"),
-                            "6,7" => array("Name"=> "FuelTrimLongTermBank2", "Units" => "%", "Conversion" => "x/10")
+                            "0" => array("Position" => "0,1", "Name"=> "FuelTrimShortTermBank1", "Units" => "%", "Conversion" => "x/10"),
+                            "1" => array("Position" => "2,3", "Name"=> "FuelTrimShortTermBank1", "Units" => "%", "Conversion" => "x/10"),	
+                            "2" => array("Position" => "4,5", "Name"=> "FuelTrimLongTermBank1", "Units" => "%", "Conversion" => "x/10"),
+                            "3" => array("Position" => "6,7", "Name"=> "FuelTrimLongTermBank2", "Units" => "%", "Conversion" => "x/10")
                         ), 
                  "3E4" => array (
-                            "1:1" => array("Name"=> "ClutchSwitch", "Units" => "int", "Conversion" => "x"),
-                            "1:2" => array("Name"=> "BrakePedalSwitch", "Units" => "int", "Conversion" => "x"),
-                            "1:3" => array("Name"=> "TransientThrottleActive", "Units" => "int", "Conversion" => "x"),
-                            "1:4" => array("Name"=> "DecelCutActive", "Units" => "int", "Conversion" => "x"),
-                            "2:1" => array("Name"=> "TorqueReductionActive", "Units" => "int", "Conversion" => "x"),
-                            "2:3" => array("Name"=> "FlatShiftSwitch", "Units" => "int", "Conversion" => "x"),
-                            "2:5" => array("Name"=> "AuxRPMLimiterActive", "Units" => "int", "Conversion" => "x"),
-                            "2:7" => array("Name"=> "AntiLagActive", "Units" => "int", "Conversion" => "x"),
-                            "7:6" => array("Name"=> "BatteryLightActive", "Units" => "int", "Conversion" => "x"),
-                            "7:7" => array("Name"=> "CheckEngineLight", "Units" => "int", "Conversion" => "x")
+                            "0" => array("Position" => "1:1", "Name"=> "ClutchSwitch", "Units" => "int", "Conversion" => "x"),
+                            "1" => array("Position" => "1:2", "Name"=> "BrakePedalSwitch", "Units" => "int", "Conversion" => "x"),
+                            "2" => array("Position" => "1:3", "Name"=> "TransientThrottleActive", "Units" => "int", "Conversion" => "x"),
+                            "3" => array("Position" => "1:4", "Name"=> "DecelCutActive", "Units" => "int", "Conversion" => "x"),
+                            "4" => array("Position" => "2:1", "Name"=> "TorqueReductionActive", "Units" => "int", "Conversion" => "x"),
+                            "5" => array("Position" => "2:3", "Name"=> "FlatShiftSwitch", "Units" => "int", "Conversion" => "x"),
+                            "6" => array("Position" => "2:5", "Name"=> "AuxRPMLimiterActive", "Units" => "int", "Conversion" => "x"),
+                            "7" => array("Position" => "2:7", "Name"=> "AntiLagActive", "Units" => "int", "Conversion" => "x"),
+                            "8" => array("Position" => "7:6", "Name"=> "BatteryLightActive", "Units" => "int", "Conversion" => "x"),
+                            "9" => array("Position" => "7:7", "Name"=> "CheckEngineLight", "Units" => "int", "Conversion" => "x")
                         ), 
     
                  "3E5" => array (
-                            "0" => array("Name"=> "IgnitionSwitch", "Units" => "int", "Conversion" => "x"),
-                            "1" => array("Name"=> "TurboTimerTimeRemaining", "Units" => "int", "Conversion" => "x"),
-                            "4,5" => array("Name"=> "SteeringWheelAngle", "Units" => "int", "Conversion" => "x/10"),
-                            "6,7" => array("Name"=> "DriveshaftRPM", "Units" => "RPM", "Conversion" => "x")
+                            "0" => array("Position" => "0", "Name"=> "IgnitionSwitch", "Units" => "int", "Conversion" => "x"),
+                            "1" => array("Position" => "1", "Name"=> "TurboTimerTimeRemaining", "Units" => "int", "Conversion" => "x"),
+                            "2" => array("Position" => "4,5", "Name"=> "SteeringWheelAngle", "Units" => "int", "Conversion" => "x/10"),
+                            "3" => array("Position" => "6,7", "Name"=> "DriveshaftRPM", "Units" => "RPM", "Conversion" => "x")
                         ),     
                  "3E6" => array (
-                            "0,1" => array("Name"=> "NOSPressureSensor2", "Units" => "kPa", "Conversion" => "x*11/50"),
-                            "2,3" => array("Name"=> "NOSPressureSensor3", "Units" => "kPa", "Conversion" => "x*11/50"),
-                            "4,5" => array("Name"=> "NOSPressureSensor3", "Units" => "kPa", "Conversion" => "x*11/50"),
-                            "6,7" => array("Name"=> "TurboSpeedSensor2", "Units" => "RPM", "Conversion" => "x*10")
+                            "0" => array("Position" => "0,1", "Name"=> "NOSPressureSensor2", "Units" => "kPa", "Conversion" => "x*11/50"),
+                            "1" => array("Position" => "2,3", "Name"=> "NOSPressureSensor3", "Units" => "kPa", "Conversion" => "x*11/50"),
+                            "2" => array("Position" => "4,5", "Name"=> "NOSPressureSensor3", "Units" => "kPa", "Conversion" => "x*11/50"),
+                            "3" => array("Position" => "6,7", "Name"=> "TurboSpeedSensor2", "Units" => "RPM", "Conversion" => "x*10")
                         ),  
+    
+                //there are more arrays to add here...
+    
+    
                 );
 
 
