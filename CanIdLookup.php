2e006a479ebcc7d11550effcc10303752da65cd5
Michael <michael.chora@gmail.com>
2017-09-29 11:34:48 +1000

    Update CanIdLookup.php

--- CanIdLookup.php
+++ CanIdLookup.php
@@ -2,10 +2,10 @@
 
 $CanIdArray = array(
                 "360" => array (
-                            "0" => array("CanID" => "0,1", "Name"=> "RPM", "Units" => "RPM", "Conversion" => "x"),
-                            "2,3" => array("Name"=> "ManifoldPressure", "Units" => "kPa", "Conversion" => "x/10"),	
-                            "4,5" => array("Name"=> "ThrottlePosition", "Units" => "%", "Conversion" => "x/10"),
-                            "6,7" => array("Name"=> "CoolantPressure", "Units" => "kPa", "Conversion" => "x/10")
+                            "0" => array("Position" => "0,1", "Name"=> "RPM", "Units" => "RPM", "Conversion" => "x"),
+                            "1" => array("Position" => "2,3","Name"=> "ManifoldPressure", "Units" => "kPa", "Conversion" => "x/10"),	
+                            "2" => array("Position" => "4,5", "Name"=> "ThrottlePosition", "Units" => "%", "Conversion" => "x/10"),
+                            "3" => array("Position" => "6,7", "Name"=> "CoolantPressure", "Units" => "kPa", "Conversion" => "x/10")
                         ),
                 "361" => array (
                             "0,1" => array("Name"=> "FuelPressure", "Units" => "kPa", "Conversion" => "x/10"),
