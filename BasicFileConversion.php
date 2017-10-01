6d59e9f4aeb23a8da3c7aa7fd32e1a4602ada849
Michael Chora <michael.chora@gmail.com>
2017-10-01 13:36:54 +1100

    added new functions for data conversion

--- BasicFileConversion.php
+++ BasicFileConversion.php
@@ -2,11 +2,76 @@
 
 include_once('CanIdLookup.php');
 
+    
+function HexPosition($Number){
+    switch ($Number) {
+        case 0:
+            return 4;
+            break;
+        case 1:
+            return 5;
+            break;
+        case 2:
+            return 6;
+            break;
+        case 3:
+            return 7;
+            break;
+        case 4:
+            return 8;
+            break;
+        case 5:
+            return 9;
+            break;            
+        case 6:
+            return 10;
+            break;
+        case 7:
+            return 11;
+            break;
+        default:
+            return "error";
+    }
+    
+}
 
-function ConvertHex($CanId) {
+function Position($PositionValue){
+    
+    if (strpos($PositionValue, ',') !== false) {
+        $returnArray = array();
+        $numbers = explode(',',$PositionValue);
+        foreach($numbers as $number){
+            $Number = HexPosition($number);
+            array_push($returnArray, $Number);
+        }
+        
+        return $returnArray;
+        
+    }elseif(strpos($PositionValue, ':') !== false){
+        $returnArray = array();
+        $numbers = explode(':',$PositionValue);
+        $Number = HexPosition($numbers[0]);
+        
+        return $Number;        
+    }
+    
+    return 0;
+    
+}
+
+function ConvertHex($CanId, $array) {
+    global $CanIdArray;
+    
+    
     $ValueArray = $CanIdArray[strtoupper($CanId)];
     
-    return $CanId;
+    foreach($ValueArray as $Value){
+        $formula = Position($Value[Position]);
+        
+    }
+    
+    
+    return $formula;
 }
 
 
@@ -21,7 +86,8 @@ if ($handle) {
                 //do nothing as the values are all 0
             }else{
                     echo date('Y-m-d | h:i:suv a', $line[0]) . "$line[2] \r\n";
-                $ValueArray = $CanIdArray[strtoupper($line[2])];
+                $ValueArray = ConvertHex($line[2], $line);
+                
                 var_dump($ValueArray);
                 
                 exit;
